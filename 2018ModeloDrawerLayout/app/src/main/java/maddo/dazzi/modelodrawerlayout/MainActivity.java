cc5fb45e14b38e7200deea7d9dd35e6021dffd34
Marco Maddo <profmaddo@gmail.com>
2018-09-26 01:44:07 -0400

    App Android com Menu Drawer
    Modelo App Android com Menu Drawer para aula no curso.

---
+++ 2018ModeloDrawerLayout/app/src/main/java/maddo/dazzi/modelodrawerlayout/MainActivity.java
@@ -0,0 +1,166 @@
+package maddo.dazzi.modelodrawerlayout;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    // TODO: Entender a injeção do MENU Drawer no Layout do projeto
+    // TODO: Entender o XML com os itens do MENU
+    // TODO: Tornar Objetos de menu globais
+
+    NavigationView navigationView;
+
+    Menu menu;
+    MenuItem nav_preto;
+    MenuItem nav_vermelho;
+    MenuItem nav_azul;
+
+    TextView txtTitulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // TODO: Manipular objetos de Menu
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        txtTitulo = findViewById(R.id.txtTitulo);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        // TODO: opter ID para a opção selecionada no MENU DRAWER
+        if (id == R.id.nav_preto) {
+
+            // TODO: Criar objeto com menu
+            menu = navigationView.getMenu();
+
+
+            nav_preto = menu.findItem(R.id.nav_preto);
+            nav_preto.setTitle("Preto Ativado");
+
+            nav_vermelho = menu.findItem(R.id.nav_vermelho);
+            nav_vermelho.setTitle("Vermelho");
+
+            nav_azul = menu.findItem(R.id.nav_azul);
+            nav_azul.setTitle("Azul");
+
+            navigationView.setItemTextColor(ColorStateList.valueOf(Color.BLACK));
+
+            txtTitulo.setTextColor(ColorStateList.valueOf(Color.BLACK));
+
+        } else if (id == R.id.nav_vermelho) {
+
+            menu = navigationView.getMenu();
+
+            nav_preto = menu.findItem(R.id.nav_preto);
+
+            // TODO: Mudas o texto do menu programaticamente
+            nav_preto.setTitle("Preto");
+
+            nav_vermelho = menu.findItem(R.id.nav_vermelho);
+            nav_vermelho.setTitle("Vermelho Ativado");
+
+            nav_azul = menu.findItem(R.id.nav_azul);
+            nav_azul.setTitle("Azul");
+
+            // TODO: Mudar a cor de todos os itens do menu programaticamente
+            navigationView.setItemTextColor(ColorStateList.valueOf(Color.RED));
+
+            txtTitulo.setTextColor(ColorStateList.valueOf(Color.RED));
+
+        } else if (id == R.id.nav_azul) {
+
+            menu = navigationView.getMenu();
+
+            nav_preto = menu.findItem(R.id.nav_preto);
+            nav_preto.setTitle("Preto");
+
+            nav_vermelho = menu.findItem(R.id.nav_vermelho);
+            nav_vermelho.setTitle("Vermelho");
+
+            nav_azul = menu.findItem(R.id.nav_azul);
+            nav_azul.setTitle("Azul Ativado");
+
+            navigationView.setItemTextColor(ColorStateList.valueOf(Color.BLUE));
+
+            txtTitulo.setTextColor(ColorStateList.valueOf(Color.BLUE));
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
