5473ae5f52b93aa34e9da6d70d9f0a0786e9f9c9
Marco Maddo <profmaddo@gmail.com>
2018-09-26 01:33:02 -0400

    Interfaces em Java
    Exemplo de uso de interaces em Java

---
+++ BonusPoo_interface/app/src/main/java/android/curso/bonuspoo/controller/VeiculoInterface.java
@@ -0,0 +1,13 @@
+package android.curso.bonuspoo.controller;
+
+/**
+ * Created by marcomaddo on 27/10/2017.
+ */
+
+public interface VeiculoInterface {
+
+    public void opcionalGPS();
+
+    public void opcionalArCondicionado();
+
+}
---
+++ BonusPoo_interface/app/src/main/java/android/curso/bonuspoo/datamodel/CarroDataModel.java
@@ -0,0 +1,10 @@
+package android.curso.bonuspoo.datamodel;
+
+/**
+ * Created by marcomaddo on 27/10/2017.
+ */
+
+public class CarroDataModel {
+
+    // definições da tabela.
+}
