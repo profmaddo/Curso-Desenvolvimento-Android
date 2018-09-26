c57fd6bebe11bbd4f75d96b8a120a34427b78404
Marco Maddo <profmaddo@gmail.com>
2018-09-26 02:19:30 -0400

    App MediaEscolar MVC CRUD Deletar SQLite
    Exemplo do método deletar com SQlite.

---
+++ Fontes-AppMediaEscolar-Aula-MVC-CRUD-Deletar/app/src/main/java/android/curso/mediaescolarmvc/view/LancarNotasActivity.java
@@ -0,0 +1,14 @@
+package android.curso.mediaescolarmvc.view;
+
+import android.curso.mediaescolarmvc.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LancarNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lancar_notas);
+    }
+}
