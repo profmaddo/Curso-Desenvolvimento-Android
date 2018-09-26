4b9b962e3889300d16765ba48923a182d53c9769
Marco Maddo <profmaddo@gmail.com>
2018-09-26 01:16:31 -0400

    Classe Contato do Primeiro App
    Classe Contato do Primeiro App

---
+++ Fontes_AulaApp01_POO/app/src/main/java/android/curso/aulaapp01/Contato.java
@@ -0,0 +1,30 @@
+package android.curso.aulaapp01;
+
+/**
+ * Created by marcomaddo on 24/10/2017.
+ */
+
+// O nome de uma classe deve sempre iniciar
+// com uma letra maiúscula
+
+public class Contato { // nome da classe
+
+    // propriedades
+
+    // Os nomes das propriedades da classe de seguir a regra
+    // CamalCase.
+    // https://pt.wikipedia.org/wiki/CamelCase
+
+    public String primeiroNome;
+    public String segundoNome;
+    public String telefone;
+    public String email;
+
+    // ações ou métodos que os objetos da classe sabem executar
+
+    // Para dar nomes aos métodos, use verbos
+    public void salvar(){}
+    public void limpar(){}
+
+
+}
