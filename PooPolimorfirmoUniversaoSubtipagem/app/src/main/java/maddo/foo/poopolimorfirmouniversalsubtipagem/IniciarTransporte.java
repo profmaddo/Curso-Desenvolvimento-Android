b49f0627f5e2e9148a5606ad75173569951e4860
Marco Maddo <profmaddo@gmail.com>
2019-05-11 15:27:43 -0400

    Update projeto Poo Polimorfismo Universal Sub-Classes, recompilado no Android Studio 3.4.

--- PooPolimorfirmoUniversaoSubtipagem/app/src/main/java/maddo/foo/poopolimorfirmouniversalsubtipagem/IniciarTransporte.java
+++
@@ -1,9 +0,0 @@
-package maddo.foo.poopolimorfirmouniversalsubtipagem;
-
-public class IniciarTransporte {
-
-
-    public void criarUmTransporte(Transporte transporte){
-        transporte.tipo();
-    }
-}
