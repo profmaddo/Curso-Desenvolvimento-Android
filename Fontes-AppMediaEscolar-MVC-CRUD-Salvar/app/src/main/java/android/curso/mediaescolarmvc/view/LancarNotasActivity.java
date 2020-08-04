360a2e4ff6a2b23c12ce5a2fd96675207e5ae121
profmaddo <profmaddo@gmali.com>
2020-08-04 16:03:22 -0300

    Atualização Android Studio 4.0.1 - API 30

--- Fontes-AppMediaEscolar-MVC-CRUD-Salvar/app/src/main/java/android/curso/mediaescolarmvc/view/LancarNotasActivity.java
+++ Fontes-AppMediaEscolar-MVC-CRUD-Salvar/app/src/main/java/android/curso/mediaescolarmvc/view/LancarNotasActivity.java
@@ -1,7 +1,7 @@
 package android.curso.mediaescolarmvc.view;
 
 import android.curso.mediaescolarmvc.R;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LancarNotasActivity extends AppCompatActivity {
