cabba25d6509c1f44e6d62f1dc66538af32a282d
profmaddo <profmaddo@gmali.com>
2020-08-04 15:20:04 -0300

    Atualização Android Studio 4.0.1 - API 30

--- App_Fonte_Externa_TrueType/app/src/main/java/foo/daazi/appfonteexternatruetype/MainActivity.java
+++ App_Fonte_Externa_TrueType/app/src/main/java/foo/daazi/appfonteexternatruetype/MainActivity.java
@@ -1,7 +1,7 @@
 package foo.daazi.appfonteexternatruetype;
 
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
