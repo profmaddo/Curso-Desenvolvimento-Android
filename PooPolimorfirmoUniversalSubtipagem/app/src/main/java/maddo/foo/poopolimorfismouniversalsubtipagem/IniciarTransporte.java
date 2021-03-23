f25759989b23a3064d23a51fa0aa84eb39411ad5
Professor Marco Maddo <profmaddo@gmail.com>
2021-03-23 12:13:48 -0400

    Atualização de versão da API 28 para 30.

--- PooPolimorfirmoUniversalSubtipagem/app/src/main/java/maddo/foo/poopolimorfismouniversalsubtipagem/IniciarTransporte.java
+++
@@ -1,9 +0,0 @@
-package maddo.foo.poopolimorfismouniversalsubtipagem;
-
-public class IniciarTransporte {
-
-
-    public void criarUmTransporte(Transporte transporte) {
-        transporte.tipo();
-    }
-}
