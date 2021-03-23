c5793e3270189b37e7088b851db6a911545aa595
Professor Marco Maddo <profmaddo@gmail.com>
2021-03-23 12:05:50 -0400

    Nome da pasta renomeado

---
+++ PooPolimorfismoUniversalSubtipagem/app/src/main/java/maddo/foo/poopolimorfismouniversalsubtipagem/IniciarTransporte.java
@@ -0,0 +1,9 @@
+package maddo.foo.poopolimorfismouniversalsubtipagem;
+
+public class IniciarTransporte {
+
+
+    public void criarUmTransporte(Transporte transporte) {
+        transporte.tipo();
+    }
+}
---
+++ PooPolimorfismoUniversalSubtipagem/app/src/main/java/maddo/foo/poopolimorfismouniversalsubtipagem/Trem.java
@@ -0,0 +1,11 @@
+package maddo.foo.poopolimorfismouniversalsubtipagem;
+
+import android.util.Log;
+
+public class Trem extends Transporte {
+
+    public void tipo() {
+        Log.i("Polimorfismo", "Todo TREM transporta pelos tilhos!");
+    }
+
+}
