03f848787977ce06004f10d558dff2460b1460f1
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-23 20:24:41 -0400

    Exemplo de Log como usar

--- PooPolimorfismoUniversalSubtipagem/app/src/main/java/maddo/foo/poopolimorfismouniversalsubtipagem/MainActivity.java
+++ PooPolimorfismoUniversalSubtipagem/app/src/main/java/maddo/foo/poopolimorfismouniversalsubtipagem/MainActivity.java
@@ -2,6 +2,8 @@ package maddo.foo.poopolimorfismouniversalsubtipagem;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,22 +16,28 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnIniciar;
 
+    String TAG = "@marcomaddo";
+    String TAG_E = "@marcomaddoerro";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"Teste de LOG para DEBUG...");
+
 
         btnIniciar = findViewById(R.id.btnIniciar);
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                Log.e(TAG,"Teste de LOG para INFO Click...");
 
                 transportarAlgo();
 
             }
         });
-
 
     }
 
@@ -43,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         obj.criarUmTransporte(new Carro());
         obj.criarUmTransporte(new Trem());
 
+        Log.w(TAG,"Teste de LOG para Warning no Método");
 
     }
-
 
 }
