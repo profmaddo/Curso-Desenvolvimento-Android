153555eda90c8d3415da2847f816fcb3b3d3e29f
marcomaddo <profmaddo@gmail.com>
2025-02-11 14:25:33 -0400

    Upgrade Android Studio Ladybug

---
+++ FontesAula01POO/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "foo.maddo.fontesaula01poo"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "foo.maddo.fontesaula01poo"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ FontesAula01POO/app/src/main/java/foo/maddo/fontesaula01poo/MainActivity.java
@@ -0,0 +1,80 @@
+package foo.maddo.fontesaula01poo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView txtTitulo;
+    EditText primeiroNome, segundoNome, telefone, email;
+    Button btnLimpar, btnSalvar;
+
+    Contato contato;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        primeiroNome = findViewById(R.id.primeiroNome);
+        segundoNome = findViewById(R.id.segundoNome);
+        telefone = findViewById(R.id.telefone);
+        email = findViewById(R.id.email);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnLimpar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        primeiroNome.setText("");
+                        segundoNome.setText("");
+                        telefone.setText("");
+                        email.setText("");
+                    }
+                });
+
+        btnSalvar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Toast.makeText(getApplicationContext(),"Primeiro nome: "+primeiroNome.getText(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"Segundo nome: "+segundoNome.getText(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"Telefone: "+telefone.getText(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"Email: "+email.getText(),Toast.LENGTH_LONG).show();
+
+                        contato = new Contato();
+
+                        contato.salvar();
+                        contato.limpar();
+
+                        Contato maddo = new Contato();
+                        Contato joana = new Contato();
+                        Contato objMaria = new Contato();
+
+                    }
+                });
+
+
+    }
+}
