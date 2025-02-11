155073eda6ca8d3e1d8e216084928f4e73812155
marcomaddo <profmaddo@gmail.com>
2025-02-11 12:43:24 -0400

    Upgrade Android Studio Ladybug

---
+++ POOPolimorfismoUniversalSubTipagem/app/src/main/java/foo/maddo/poopolimorfismouniversalsubtipagem/IniciarTransporte.java
@@ -0,0 +1,9 @@
+package foo.maddo.poopolimorfismouniversalsubtipagem;
+
+public class IniciarTransporte {
+
+
+    public void criarUmTransporte(Transporte transporte) {
+        transporte.tipo();
+    }
+}
