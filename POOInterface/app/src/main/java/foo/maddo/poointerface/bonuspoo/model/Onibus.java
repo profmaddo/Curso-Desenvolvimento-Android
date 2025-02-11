8d56120f27dc40164dcbbe412c5c9301f6e2646c
marcomaddo <profmaddo@gmail.com>
2025-02-11 13:22:39 -0400

    Upgrade Android Studio Ladybug

---
+++ POOInterface/app/src/main/java/foo/maddo/poointerface/bonuspoo/model/Onibus.java
@@ -0,0 +1,21 @@
+package foo.maddo.poointerface.bonuspoo.model;
+
+
+import foo.maddo.poointerface.bonuspoo.controller.VeiculoInterface;
+
+/**
+ * Created by marcomaddo on 27/10/2017.
+ */
+
+public class Onibus extends Fabricante implements VeiculoInterface {
+
+    @Override
+    public void opcionalGPS() {
+
+    }
+
+    @Override
+    public void opcionalArCondicionado() {
+
+    }
+}
